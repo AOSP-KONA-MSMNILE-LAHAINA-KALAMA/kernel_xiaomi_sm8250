c038c3ec6af113d007c32b7fbc41e5a7d705d632
Demon000 <demonsingur@gmail.com>
2020-04-05 23:43:09 +0200

    techpack: display: msm: dsi: implement doze mode
    Change-Id: I62b27deaa250ab2621be8f37474024fcdb49cce6

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -52,6 +52,11 @@ enum dsi_backlight_type {
 	DSI_BACKLIGHT_MAX,
 };
 
+enum dsi_doze_mode_type {
+	DSI_DOZE_LPM = 0,
+	DSI_DOZE_HBM,
+};
+
 enum bl_update_flag {
 	BL_UPDATE_DELAY_UNTIL_FIRST_FRAME,
 	BL_UPDATE_NONE,
@@ -235,6 +240,9 @@ struct dsi_panel {
 	int power_mode;
 	enum dsi_panel_physical_type panel_type;
 
+	bool doze_enabled;
+	enum dsi_doze_mode_type doze_mode;
+
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
 };
@@ -372,6 +380,10 @@ int dsi_panel_create_cmd_packets(const char *data,
 				struct dsi_cmd_desc *cmd);
 void dsi_panel_destroy_cmd_packets(struct dsi_panel_cmd_set *set);
 void dsi_panel_dealloc_cmd_packets(struct dsi_panel_cmd_set *set);
+
+int dsi_panel_set_doze_status(struct dsi_panel *panel, bool status);
+
+int dsi_panel_set_doze_mode(struct dsi_panel *panel, enum dsi_doze_mode_type mode);
 
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 
